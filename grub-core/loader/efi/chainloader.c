cc41635960fbfb8dcfd650038b97e9066aae20b1
Leo Sandoval <lsandova@redhat.com>
2024-02-02 12:02:05 -0600

    0208-grub-core-loader-efi-chainloader.c-do-not-validate-c.patch
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- grub-core/loader/efi/chainloader.c
+++ grub-core/loader/efi/chainloader.c
@@ -906,7 +906,6 @@ grub_cmd_chainloader (grub_command_t cmd __attribute__ ((unused)),
   grub_efi_device_path_t *dp = NULL, *file_path = NULL;
   char *filename;
   void *boot_image = 0;
-  int rc;
   grub_efi_physical_address_t address = 0;
   grub_efi_uintn_t pages = 0;
   grub_efi_char16_t *cmdline = NULL;
@@ -1078,17 +1077,15 @@ grub_cmd_chainloader (grub_command_t cmd __attribute__ ((unused)),
       orig_dev = 0;
     }
 
-  rc = grub_linuxefi_secure_validate((void *)(unsigned long)address, fsize);
-  grub_dprintf ("chain", "linuxefi_secure_validate: %d\n", rc);
-  if (rc > 0)
-    {
+  if (grub_efi_get_secureboot () == GRUB_EFI_SECUREBOOT_MODE_ENABLED)
+  {
       grub_file_close (file);
       grub_device_close (dev);
       grub_loader_set (grub_secureboot_chainloader_boot,
 		       grub_secureboot_chainloader_unload, 0);
       return 0;
     }
-  else if (rc == 0)
+  else
     {
       grub_load_and_start_image(boot_image);
       grub_file_close (file);
@@ -1096,7 +1093,6 @@ grub_cmd_chainloader (grub_command_t cmd __attribute__ ((unused)),
       grub_loader_set (grub_chainloader_boot, grub_chainloader_unload, 0);
       return 0;
     }
-  // -1 fall-through to fail
 
  fail:
   if (orig_dev)
