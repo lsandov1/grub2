0114ac979ed84ae575629f2c32bdcfeff39ea0b3
Leo Sandoval <lsandova@redhat.com>
2024-10-15 11:28:17 -0600

    kern/err.c: print all errors from the error stack
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- grub-core/kern/err.c
+++ grub-core/kern/err.c
@@ -119,6 +119,8 @@ grub_print_error (void)
       if (grub_errno != GRUB_ERR_NONE)
 	{
 	  grub_err_printf (_("error: %s.\n"), grub_errmsg);
+    grub_printf ("error:%s. \n", grub_errmsg);
+
 	  grub_err_printed_errors++;
 	}
     }
